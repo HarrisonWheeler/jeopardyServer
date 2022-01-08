82737c42fc867daa70b9bf2774fa9bfc94820e58
HarrisonWheeler <wheelerharrison93@gmail.com>
2022-01-08 10:38:22 -0700

    all crud methods tested and working

--- server/controllers/PlayersController.js
+++ server/controllers/PlayersController.js
@@ -6,15 +6,48 @@ export class PlayersController extends BaseController {
     super('api/players')
     this.router
       .get('', this.getAll)
-      .get('/:id')
-      .put('/:id')
-      .post('')
-      .delete('/:id')
+      .get('/:id', this.getById)
+      .put('/:id', this.edit)
+      .post('', this.create)
+      .delete('/:id', this.remove)
   }
 
   async getAll(req, res, next) {
     try {
       return res.send(await playersService.getAll(req.query))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      return res.send(await playersService.getById(req.params.id))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      return res.send(await playersService.create(req.body))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      return res.send(await playersService.edit(req.body))
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async remove(req, res, next) {
+    try {
+      return res.send(await playersService.remove(req.params.id))
     } catch (error) {
       next(error)
     }
--- server/services/PlayersService.js
+++ server/services/PlayersService.js
@@ -1,9 +1,39 @@
 import { dbContext } from "../db/DbContext"
+import { BadRequest } from '../utils/Errors.js'
 
 
 class PlayersService {
   async getAll(query = {}) {
     return await dbContext.Players.find(query)
+  }
+
+  async getById(id) {
+    const foundPlayer = await dbContext.Players.findById(id)
+    if (!foundPlayer) {
+      throw new BadRequest('Unable to find that player')
+    }
+    return foundPlayer
+  }
+
+  async create(newPlayer) {
+    return await dbContext.Players.create(newPlayer)
+  }
+
+  async edit(editedPlayer) {
+    let foundPlayer = await this.getById(editedPlayer.id)
+    foundPlayer.name = editedPlayer.name || foundPlayer.name
+    foundPlayer.points = editedPlayer.points || foundPlayer.points
+    foundPlayer.correct = editedPlayer.correct || foundPlayer.correct
+    foundPlayer.incorrect = editedPlayer.incorrect || foundPlayer.incorrect
+    foundPlayer.questions = editedPlayer.questions || foundPlayer.questions
+    await foundPlayer.save()
+    return foundPlayer
+  }
+
+  async remove(id) {
+    let playerToDelete = await this.getById(id)
+    await playerToDelete.remove()
+    return playerToDelete
   }
 }
 
