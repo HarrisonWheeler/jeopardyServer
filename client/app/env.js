c61d316ea39620e00335e6a28b5c3c9b1f07faf5
HarrisonWheeler <wheelerharrison93@gmail.com>
2022-01-08 12:59:49 -0700

    change

--- client/app/env.js
+++ client/app/env.js
@@ -1,2 +1,2 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'http://localhost:3000' : 'https://jeoparty-server.herokuapp.com/api/'
